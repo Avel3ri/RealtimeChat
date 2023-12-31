0d316f533c9d35a193aa229e815b03f1ddd2381f
Eryk Kunasz <eryk.kunasz@gmail.com>
2023-12-31 16:00:56 +0100

    Frontend

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,21 @@
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "67a66678-5985-48d6-812b-e59b7b3ed06c",
+    props.user.username,
+    props.user.secret
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+
+export default ChatsPage;
